dbb1a73ca72a44c83f66555eff0c8e1e04e373e7
Jérémy Brochard <jrmy.brochard@gmail.com>
2024-07-30 18:14:43 +0200

    feat: add default home page with basic routing to another about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about.component'),
+    title: 'About',
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
